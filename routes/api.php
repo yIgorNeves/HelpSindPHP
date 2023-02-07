ed70e33522712f49102d932c0c492a89f5e208b6
Igor Neves <32842877+yIgorNeves@users.noreply.github.com>
2023-02-07 07:28:01 -0300

    Controllers

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BilletController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\UnitController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WarningController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
