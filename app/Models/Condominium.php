83058334452dea1c9c7253da708171ee6cb829e5
Igor Neves <32842877+yIgorNeves@users.noreply.github.com>
2023-02-12 19:11:06 -0300

    Atualizações

--- app/Models/Condominium.php
+++ app/Models/Condominium.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Comdominium extends Model
+class Condominium extends Model
 {
     use HasFactory;
 
--- routes/api.php
+++ routes/api.php
@@ -3,24 +3,46 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AddressController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BilletController;
+use App\Http\Controllers\CondominiumController;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\UnitController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WarningController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/401', [AuthController::class, 'unauthorized'])->name('login');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/auth/validate', [AuthController::class, 'validateToken']);
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+    //Registro de ocorrencias
+    Route::get('/warnings', [WarningController::class, 'getMyWarning']);
+    Route::post('/warning', [WarningController::class, 'setWarning']);
+    Route::post('/warning/file',[WarningController::class, 'setWarningFile']);
+
+    //Boletos
+    Route::get('/billets', [BilletController::class, 'getAll']);
+
+    //Unidades
+    Route::get('/unit/{id}', [UnitController::class, 'getInfo']);
+    Route::post('/unit/{id}/addperson',[UnitController::class, 'addPerson']);
+    Route::post('/unit/{id}/removeperson',[UnitController::class, 'removePerson']);
+
+    //Reservas
+    Route::get('/reservations', [ReservationController::class, 'getReservation']);
+    Route::post('/myreservation',[ReservationController::class, 'getMyReservation']);
+
+    Route::get('/reservation/{id}/disableddates',[ReservationController::class, 'getDisabledDates']);
+    Route::get('/reservation/{id}/times', [ReservationController::class, 'getTimes']);
+
+    Route::delete('/myreservation/{id}',[ReservationController::class, 'delMyReservation']);
+    Route::post('/reservation/{id}',[ReservationController::class, 'setMyReservation']);
+
 });
+
